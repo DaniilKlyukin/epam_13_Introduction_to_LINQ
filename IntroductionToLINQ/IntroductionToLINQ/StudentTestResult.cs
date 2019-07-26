610cf0da5751aeae70af1795ffb9af3564f50187
Daniil <anatoliikljukin@mail.ru>
2019-07-26 17:15:49 +0400

    Create StudentTestResult.cs
    Class for storing data.

---
+++ IntroductionToLINQ/IntroductionToLINQ/StudentTestResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IntroductionToLINQ
+{
+    class StudentTestResult
+    {
+        public string StudentName { get; set; }
+        public string TestName { get; set; }
+        public DateTime TestDate { get; set; }
+        public int Mark { get; set; }
+
+        public StudentTestResult(string studentName, string testName, DateTime date, int mark)
+        {
+            StudentName = studentName;
+            TestName = testName;
+            TestDate = date;
+            Mark = mark;
+        }
+    }
+}
